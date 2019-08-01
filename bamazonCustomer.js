63d0c57d12a39ae08411b50db04830af1f859f2e
jess <jlfinlayson4@gmail.com>
2019-08-01 18:25:31 -0400

    can console.log product_name or chosenItem

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,21 +8,44 @@ var connection = mysql.createConnection({
     user: "root",
     password: "password",
     database: "bamazon_DB"
-  });
+});
 
 // Connecting to mysql server and database
-connection.connect(function(err){
+connection.connect(function (err) {
     if (err) throw err;
-    start();
+    showTable();
 });
 
 // Function to show the table and ask questions
-function start(){
-    connection.query("SELECT * FROM products", function(err, results){
+function showTable() {
+    connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
         for (var i = 0; i < results.length; i++) {
-            console.log(results[i].product_name + " | " + results[i].department_name + " | " + results[i].price + " | " + results[i].stock_quantity);
+            console.log(results[i].item_id + " | " + results[i].product_name + " | " + results[i].department_name + " | " + results[i].price + " | " + results[i].stock_quantity);
         }
-    });
+    inquirer
+        .prompt([
+            {
+                name: "itemID",
+                type: "input",
+                message: "What is the id of the item you would like to purchase?",
+            },
+            // {
+            //     name: "quantity",
+            //     type: "input",
+            //     message: "How many would you like to purchase?",
+            // }
+        ])
+        .then(function (answer) {
+            // console.log(answer.itemID)
+            var chosenItem;
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].item_id == answer.itemID) {
+                    chosenItem = results[i];
+                    console.log(chosenItem.product_name);
+                }
+            }
+        })
     connection.end();
+});
 }
