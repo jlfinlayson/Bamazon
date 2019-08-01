fbec25b8bbeda256c00f96b0a83d3becd420c8f1
jess <jlfinlayson4@gmail.com>
2019-08-01 19:24:12 -0400

    Fully functioning bamazon

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,39 +13,77 @@ var connection = mysql.createConnection({
 // Connecting to mysql server and database
 connection.connect(function (err) {
     if (err) throw err;
-    showTable();
+    start();
 });
 
 // Function to show the table and ask questions
-function showTable() {
+function start() {
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
         for (var i = 0; i < results.length; i++) {
             console.log(results[i].item_id + " | " + results[i].product_name + " | " + results[i].department_name + " | " + results[i].price + " | " + results[i].stock_quantity);
         }
-    inquirer
-        .prompt([
-            {
-                name: "itemID",
-                type: "input",
-                message: "What is the id of the item you would like to purchase?",
-            },
-            // {
-            //     name: "quantity",
-            //     type: "input",
-            //     message: "How many would you like to purchase?",
-            // }
-        ])
-        .then(function (answer) {
-            // console.log(answer.itemID)
-            var chosenItem;
-            for (var i = 0; i < results.length; i++) {
-                if (results[i].item_id == answer.itemID) {
-                    chosenItem = results[i];
-                    console.log(chosenItem.product_name);
+        inquirer
+            .prompt([
+                {
+                    name: "itemID",
+                    type: "input",
+                    message: "What is the id of the item you would like to purchase?",
+                },
+                {
+                    name: "quantity",
+                    type: "input",
+                    message: "How many would you like to purchase?",
                 }
-            }
-        })
-    connection.end();
-});
+            ])
+            .then(function (answer) {
+                // gets the information of the chosen item
+                var chosenItem;
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].item_id == answer.itemID) {
+                        chosenItem = results[i];
+                    }
+                }
+
+                // determines if there is enough inventory
+                if (chosenItem.stock_quantity < answer.quantity) {
+                    console.log("Insufficient quantity!");
+                    connection.end();
+                } else {
+                    var totalPrice = (chosenItem.price * answer.quantity);
+                    inquirer
+                        .prompt([
+                            {
+                                name: "confirm",
+                                type: "list",
+                                message: ("Are you sure you want to buy " + answer.quantity + " " + chosenItem.product_name + "'s for $" + totalPrice + "?"),
+                                choices: ["YES", "NO"],
+                            }
+                        ])
+                        .then(function (user) {
+                            if (user.confirm === "YES") {
+                                connection.query(
+                                    "UPDATE products SET ? WHERE ?",
+                                    [
+                                        {
+                                            stock_quantity: (chosenItem.stock_quantity - answer.quantity)
+                                        },
+                                        {
+                                            item_id: chosenItem.item_id
+                                        }
+                                    ],
+                                    function (error) {
+                                        if (error) throw err;
+                                        console.log("There are now " + chosenItem.stock_quantity + " " + chosenItem.product_name + "s in stock.");
+
+                                        connection.end();
+                                    }
+
+                                );
+
+                            }
+                        })
+                }
+            })
+    });
 }
