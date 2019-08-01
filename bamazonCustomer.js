540d9088bba76eab3186f4529c56318b3f695afb
jess <jlfinlayson4@gmail.com>
2019-08-01 17:48:43 -0400

    connected to database, function to console.log products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,28 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+// Information to connect to mysql database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazon_DB"
+  });
+
+// Connecting to mysql server and database
+connection.connect(function(err){
+    if (err) throw err;
+    start();
+});
+
+// Function to show the table and ask questions
+function start(){
+    connection.query("SELECT * FROM products", function(err, results){
+        if (err) throw err;
+        else {
+            console.log(results);
+        }
+    });
+    connection.end();
+}
