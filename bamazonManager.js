ff4a413adb72fdcd16d32c152abecb60ea4188b0
jess <jlfinlayson4@gmail.com>
2019-08-01 20:41:44 -0400

    view products and low inventory functions created

--- bamazonManager.js
+++ bamazonManager.js
@@ -17,3 +17,70 @@ connection.connect(function (err) {
     if (err) throw err;
     start();
 });
+
+function start() {
+    inquirer
+        .prompt({
+            name: "managerOptions",
+            type: "list",
+            message: "Would you like to : ",
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add a New Product"]
+        })
+        .then(function (answer) {
+            if (answer.managerOptions === "View Products for Sale") {
+                viewProducts();
+            }
+            else if (answer.managerOptions === "View Low Inventory") {
+                lowInventory();
+            }
+            //else if (answer.managerOptions === "Add to Inventory") {
+            //     addInventory();
+            // } 
+            //else if (answer.managerOptions === "Add a New Product") {
+            //     addNewProduct();
+            // }
+        });
+}
+
+function viewProducts() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        var table = new Table({
+            head: ["Id#", "Product Name", "Department", "Price", "Quantity"],
+            style: {
+                head: ["bold", "green"],
+                border: ["green"],
+            }
+        });
+        for (var i = 0; i < results.length; i++) {
+            table.push(
+                [results[i].item_id, results[i].product_name, results[i].department_name, "$" + results[i].price, results[i].stock_quantity]
+            );
+        }
+        console.log(table.toString());
+    })
+    connection.end();
+}
+
+function lowInventory() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        var table = new Table({
+            head: ["Id#", "Product Name", "Department", "Price", "Quantity"],
+            style: {
+                head: ["bold", "green"],
+                border: ["green"],
+            }
+        });
+        for (var i = 0; i < results.length; i++) {
+            if (results[i].stock_quantity <= 5){
+            table.push(
+                [results[i].item_id, results[i].product_name, results[i].department_name, "$" + results[i].price, results[i].stock_quantity]
+            );
+        }
+    }
+        console.log(table.toString());
+    })
+
+    connection.end();
+}
